3cd9d57ae39e6ebbb05752af139ae8b21edacebb
Sapphire Cat <devel@sapphirepaw.org>
2022-09-02 13:51:45 -0400

    Add a script to build GitHub files into dist

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -eu
+
+cd "$(dirname "$0")"
+
+# make sure we are probably in a reasonable place
+if [ ! -d src ] ; then
+    echo "src: not a directory" >&2
+    exit 1
+fi
+
+# clear dist dir, safely
+if [ -d dist ] ; then
+    rm -rf dist
+elif [ -e dist ] ; then
+    echo "dist: not a directory" >&2
+    exit 1
+fi
+
+mkdir dist
+cd src
+cp -a ./*.html ./*.css ./*.js pickr ./*.svg ../dist
